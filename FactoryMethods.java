7bb0e378092c8e2b2b0ed56df9f88b01ed55dca7
Andrés Felipe Rincón Gamboa <andres.rincon@mail.mcgill.ca>
2020-10-22 23:50:13 -0400

    Create FactoryMethods.java

---
+++ FactoryMethods.java
@@ -0,0 +1,8 @@
+package com.effectivejava.items.item2.FactoryMethods;
+
+public final class FactoryMethods {
+
+    public static Boolean valueOf(boolean bool) {
+        return bool ? Boolean.TRUE : Boolean.FALSE;
+    }
+}
