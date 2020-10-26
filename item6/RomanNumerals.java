90f399e150d9beb0f2c4fb0b57b1cc902bcfe9cd
Andrés Felipe Rincón Gamboa <andres.rincon@mail.mcgill.ca>
2020-10-26 15:54:41 -0400

    Create RomanNumerals.java

---
+++ item6/RomanNumerals.java
@@ -0,0 +1,13 @@
+package com.effectivejava.items.item6;
+
+import java.util.regex.Pattern;
+
+public class RomanNumerals {
+
+    private static final Pattern ROMAN = Pattern.compile("^(?=.)M*(C[MD]|D?C{0,3})"
+            + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    public static boolean isRomanNumeral(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+}
