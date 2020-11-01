2f721ba671536afc004badcd44b8c1b852415717
Andrés Felipe Rincón Gamboa <andres.rincon@mail.mcgill.ca>
2020-10-31 22:59:47 -0400

    Create Endpoints.java

---
+++ utility/security/Endpoints.java
@@ -0,0 +1,19 @@
+package com.effectivejava.items.utility.security;
+
+public final class Endpoints {
+
+    public static final String LOGIN_VIEW_NAME = "login";
+
+    public static final String LOGIN_URL = "/" + LOGIN_VIEW_NAME;
+
+    public static final String LOGOUT_URL = "/logout";
+
+    public static final String REGISTRATION_VIEW_NAME = "registration";
+
+    public static final String REGISTRATION_URL = "/" + REGISTRATION_VIEW_NAME;
+
+    public static final String ADMIN_VIEW_NAME = "admin";
+
+    public static final String ADMIN_BASE_PATH = "/" + ADMIN_VIEW_NAME;
+
+}
