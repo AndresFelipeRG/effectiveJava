70578b3e6ea2ba9bd11e54d806c86e4fa855b770
Andrés Felipe Rincón Gamboa <andres.rincon@mail.mcgill.ca>
2020-11-02 23:30:58 -0500

    Create MessagesProperties.java

---
+++ components/holder/MessagesProperties.java
@@ -0,0 +1,14 @@
+package com.effectivejava.items.utils.components.holder;
+
+public interface MessagesProperties {
+
+    public interface Messages {
+
+       
+
+        String getAttemptCountExceed();
+
+
+    }
+
+}
